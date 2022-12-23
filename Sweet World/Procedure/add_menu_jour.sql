3d881372c86c97d4543f4173969f3e4a3a3e6dd1
Danielle Stone Awounang <stonetemfack@gmail.com>
2022-12-23 03:08:28 +0100

    Update add_menu_jour.sql

--- Sweet World/Procedure/add_menu_jour.sql
+++ Sweet World/Procedure/add_menu_jour.sql
@@ -3,24 +3,21 @@ declare
     v_dish_id Dish.id_dish%type;
     v_seculant_id Seculant.id_seculant%type;
     v_id_complement Complement.id_complement%type;
+    v_id_resto Restaurant.id_resto%type;
     v_option varchar(50);
     choix char ;
 
-    cursor v_menu is
-    select id_menu, Menu_title, Menu_description,
-        Menu_price, Menu_date, Menu_Qty, id_dish,
-        id_seculant, id_complement
-    from Menu ;
-    
-    cursor c_cursor is
-    select username, password from Users 
-    where username = '&username'
-        and password = '&password';
-
 begin
 
-    open v_menu;
-    open c_cursor;
+    select id_resto
+        into v_id_resto
+        from Restaurant r
+        join Users u
+        on r.id_user=u.id_user
+        where u.username='&username'
+        and u.password='&password';
+
+  IF sql%found THEN
 
     select id_dish
     into v_dish_id
@@ -37,29 +34,20 @@ begin
     from Complement
     where Name_complement = '&complement_name';
 
-  IF c_cursor%found THEN
 
-    insert into v_menu 
+    insert into Menu 
     (
         id_menu, Menu_title, Menu_description,
         Menu_price, Menu_date, Menu_Qty, id_dish,
-        id_seculant, id_complement
+        id_seculant, id_complement, id_resto, id_config
     )
     values
     (
         id_menu_seq.nextval,'&titre','&description',
         &price, to_date(sysdate,'dd-mm-yy'),
         &quantite, v_dish_id, v_seculant_id,
-        v_id_complement
+        v_id_complement, v_id_resto, 1
     );
-
-    FOR menu_record IN v_menu
-    LOOP
-    DBMS_OUTPUT.PUT_LINE( v_menu.id_menu
-    ||' ' ||v_menu.Menu_title||' '||v_menu.Menu_description
-    ||' '||v_menu.Menu_price||' '||v_menu.Menu_date||' '||
-    v_menu.Menu_Qty); 
-    END LOOP;
 
     DBMS_OUTPUT.PUT_LINE('**********************************************'); 
     DBMS_OUTPUT.PUT_LINE('*               1. Retour                    *') ;

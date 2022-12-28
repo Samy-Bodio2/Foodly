9b36a38e701a7b89a8163a617bdcc69a7bd41595
JasonKsj <jasonkamsu@gmail.com>
2022-12-28 09:38:15 +0100

    correction

--- Sweet World/Object/Tables/Table_user.sql
+++ Sweet World/Object/Tables/Table_user.sql
@@ -10,6 +10,7 @@ CREATE TABLE Users
 (
    id_user  INT         NOT NULL,
    username VARCHAR(25) NOT NULL,
-   password VARCHAR(9)  NOT NULL
+   password VARCHAR(9)  NOT NULL,
+   id_admin INT
 );
  --***********BY TATSINKOU**********--

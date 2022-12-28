7ab23013c58235a70abeab61afb4f7216ebb3374
Samy bodio <115032144+Samy-Bodio@users.noreply.github.com>
2022-12-28 03:06:50 +0100

    Makolo fait gaffe a ton dossier

--- Sweet World/Procedure/Annaelle/consult_details_menu.sql
+++ Sweet World/Procedure/Annaelle/consult_details_menu.sql
@@ -1,23 +1,29 @@
-Select Menu_title,
-       Menu_description,
-       Menu_price,
-       Menu_date,
-       Menu_Qty,
-       D.Name_dish,
-       S.Name_Seculant,
-      C.Name_complement
-From Menu M
-   join Dish D
-   on M.id_dish = D.id_dish
-   join Complement C
-   on C.id_complement = M.id_complement
-   join Seculant S
-   on S.id_seculant = M.id_seculant
-   join Restaurant R
-   on R.id_resto = M.id_resto
-WHERE M.Menu_title = '&Menu_title';
-
-
-
-
-
+CREATE PROCEDURE consult_details_menu 
+r_id_menu            restaurant.restaurant_id%TYPE;
+r_Menu_title         restaurant.restaurant_title%TYPE;
+r_Menu_descrption    restaurant.restaurant_description%TYPE;
+r_Menu_price         restaurant.restaurant_price%TYPE;
+r_Menu_date          restaurant.restaurant_date%TYPE;
+r_Menu_Qty           restaurant.restaurant_quantity%TYPE;
+r_id_dish            restaurant.restaurant_id%TYPE;
+r_id_complement      restaurant.restaurant_id%TYPE;
+r_id_secilant        restaurant.restaurant_id%TYPE;
+r_Name_dish          restaurant.restaurant_name%TYPE;
+r_Name_complement    restaurant.restaurant_name%TYPE;
+r_Name_seculant      restaurant.restaurant_name%TYPE;
+IS 
+BEGIN
+SELECT
+    id_menu,           
+    Menu_title,         
+    Menu_descrption,   
+    Menu_price,        
+    Menu_date,         
+    Menu_Qty,           
+    id_dish,            
+    id_complement,     
+    id_secilant,       
+    Name_dish,          
+    Name_complement ,  
+    Name_seculant     
+FROM  Menu;

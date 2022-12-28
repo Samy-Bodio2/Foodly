fea58fc33944ca5127ab7a33b6bc4097622f18f0
manuelfotso <manuelfotso93@gmail.com>
2022-12-28 19:39:22 +0100

    ajout

--- Sweet World/Procedure/Manuel/add_restaurant_authentification.sql
+++ Sweet World/Procedure/Manuel/add_restaurant_authentification.sql
@@ -1,34 +1,39 @@
-CREATE TABLE RESTO AS SELECT * FROM Restaurant;
-create procedure add_restaurant_authentification  IS
-r_id_restaurant      Restaurant.restaurant_id%TYPE;
-r_Name_resto         Restaurant.restaurant_name%TYPE;
-r_Name_employees     Restaurant.restaurant_name%TYPE;
-r_Description        Restaurant.restaurant_descrption%TYPE;
-r_Phone_number       Restaurant.restaurant_number%TYPE;
-r_Resto_adresse      Restaurant.restaurant_adresse%TYPE;
-r_Date_res_created   Restaurant.restaurant_date%TYPE;
-r_id_user            Restaurant.restaurant_id%TYPE;
+
+create  or replace procedure add_restaurant_authentification  IS
+DECLARE
+test ADMIN.name_admin%TYPE;
+test1 ADMIN.pwd_admin%TYPE;
+    CURSOR c_cursor IS
+    SELECT name_admin, pwd_admin FROM ADMIN WHERE name_admin = '&name_admin' and pwd_admin = '&password';
+r_id_restaurant      Restaurant.id_resto%TYPE;
+r_Name_resto         Restaurant.Name_resto%TYPE;
+r_Name_employees     Restaurant.Name_employees%TYPE;
+r_Description        Restaurant.Description%TYPE;
+r_Phone_number       Restaurant.Phone_number%TYPE;
+r_Resto_adresse      Restaurant.Resto_adresse%TYPE;
+r_Date_res_created   Restaurant.Date_res_created%TYPE;
+
 BEGIN 
   INSERT INTO Restaurant
   (
-    [id_restaurant],
-     [Name_resto],
-     [Name_employees],
-     [Description],
-     [Phone_number],
-     [Date_res_created],
-     [id_user]
+    id_resto,
+     Name_resto,
+     Name_employees,
+     Description,
+     Phone_number,
+     Date_res_created,
+     id_user
   )
   VALUES 
   (
-    r_id_restaurant ,
+    r_id_resto_seq.nextval ,
     r_Name_resto,
     r_Name_employees,
     r_Description,
     r_Phone_number,
     r_Resto_adresse,
     r_Date_res_created ,
-    r_id_user          
+    r_id_user_seq.nextval          
   );
   END;
   /

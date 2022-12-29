15caaf5801d3f3a4f7c97c6b492b300a4cf45131
Tatsinkou <tatsinkoucrispin46@gmail.com>
2022-12-29 07:02:19 +0100

    Modification des procedures

--- Sweet World/Procedure/TATSINKOU/Procedure_modif_menu.sql
+++ Sweet World/Procedure/TATSINKOU/Procedure_modif_menu.sql
@@ -15,6 +15,33 @@ v_chaine            VARCHAR(30)='Modifier votre Menu '
 BEGIN
 DBMS_OUTPUT.PUT_LINE(v_chaine);
 END ;
+select id_resto
+        into v_id_resto
+        from Restaurant r
+        join Users u
+        on r.id_user=u.id_user
+        where u.username='&username'
+         and u.password='&passwd';
+
+  IF sql%found THEN
+
+    select id_dish
+    into v_dish_id
+    from Dish
+    where Name_dish = '&dish_name';
+
+    select id_seculant
+    into v_seculant_id
+    from Seculant
+    where Name_Seculant = '&seculant_name';
+
+    select id_complement
+    into v_id_garnish
+    from Complement
+    where Name_complement = '&Name_garnish';
+
+    IF sql%found THEN
+
 UPDATE Menu SET
  Menu_title          = v_ Menu_title ,      
     Menu_description = v_Menu_description ,
@@ -27,6 +54,28 @@ UPDATE Menu SET
     id_resto         = v_id_resto,
     id_config        = v_id_config
 WHERE id_menu        =v_id_menu;
+
+ DBMS_OUTPUT.PUT_LINE ('Voulez-vous modifier un autre menu ? (y/n)');
+    if choix1 = 'y' 
+    then DBMS_output.put_line('Modification effectuée');
+    elsif choix1 = 'n'
+    then
+        DBMS_OUTPUT.PUT_LINE ('1. Retour');
+        DBMS_OUTPUT.PUT_LINE ('2. Quitter');
+    Else
+        elsif v_option = '1' then DBMS_output.put_line('Retour');
+        elsif v_option = '2' then DBMS_output.put_line('Quitter');
+        else DBMS_output.put_line('Vous devez faire un choix valide');
+        end if;
+    end if;
+
+    else 
+    DBMS_OUTPUT.PUT_LINE('votre login et votre mot de passe ne sont pas correctes. Entrez des informations justes');
+    end if;
+
+    exception
+    when no_data_found then
+    DBMS_OUTPUT.PUT_LINE('aucune valeur trouvée');
 END ;
 /
 -------******BY TATSINKOU******-------

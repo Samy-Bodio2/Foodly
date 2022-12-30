c500c26a4f0754adc51aca23986044b53783d95b
Samy bodio <115032144+Samy-Bodio@users.noreply.github.com>
2022-12-30 12:59:45 +0100

    modif delete menu panier

--- Sweet World/Procedure/Samy/delete_menu_panier.sql
+++ Sweet World/Procedure/Samy/delete_menu_panier.sql
@@ -3,27 +3,43 @@ CREATE OR REPLACE FUNCTION custFonc(varNom VARCHAR)
 RETURN INT IS
     valt Users.id_user%TYPE;
     valt2 Customers%ROWTYPE;
+    valt3 Users.username%TYPE;
 
+    entier int := id_customers_seq.currval;
     CURSOR iduser IS
     SELECT id_user from Users where username = varNom;
+
+    CURSOR idusername IS
+    SELECT U.username from Users U
+    JOIN Customers C
+    ON C.id_user = U.id_user
+    where C.id_user = (
+        SELECT id_user from Users where username = varNom
+    );
 
     CURSOR custs IS
     SELECT * from Customers;
 BEGIN
+    OPEN idusername;
+    FETCH idusername INTO valt3;
+    IF valt3 = varNom THEN
     OPEN iduser;
     OPEN custs;
     FETCH iduser INTO valt;
 
-    LOOP
-        FETCH custs INTO valt2;
-        EXIT WHEN valt2.id_user = valt;
-            
+    FOR i IN 1..entier LOOP
+            FETCH custs INTO valt2;
+            EXIT WHEN valt2.id_user = valt;
     END LOOP;
-    DBMS_OUTPUT.PUT_LINE('Nom du Menu:'||valt2.id_user);
-
-    RETURN valt2.id_user;
+    DBMS_OUTPUT.PUT_LINE('Reussite de la Suppression'||valt2.id_cust);
+    RETURN valt2.id_cust;
+    ELSE
+    DBMS_OUTPUT.PUT_LINE('Echec de la Suppression');
+    RETURN 0;
+    END if;
 END;
 /
+
 DECLARE
 PROCEDURE operation
 AS
@@ -57,6 +73,8 @@ BEGIN
     SET Menu_Qty = Menu_Qty + valt
     WHERE id_menu = valt2;
     DELETE FROM Panier
+    WHERE id_cust = varnomFonc;
+    DELETE FROM Customers
     WHERE id_cust = varnomFonc;
 END;
 BEGIN

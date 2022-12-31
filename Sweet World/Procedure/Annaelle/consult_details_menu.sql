f847be712c8190de23cd59bd687146a3249d35c6
annaellejosee13 <annaellejoseemegane@gmail.com>
2022-12-31 01:37:37 +0100

    add_resto

--- Sweet World/Procedure/Annaelle/consult_details_menu.sql
+++ Sweet World/Procedure/Annaelle/consult_details_menu.sql
@@ -1,29 +1,336 @@
-CREATE PROCEDURE consult_details_menu 
-r_id_menu            restaurant.restaurant_id%TYPE;
-r_Menu_title         restaurant.restaurant_title%TYPE;
-r_Menu_descrption    restaurant.restaurant_description%TYPE;
-r_Menu_price         restaurant.restaurant_price%TYPE;
-r_Menu_date          restaurant.restaurant_date%TYPE;
-r_Menu_Qty           restaurant.restaurant_quantity%TYPE;
-r_id_dish            restaurant.restaurant_id%TYPE;
-r_id_complement      restaurant.restaurant_id%TYPE;
-r_id_secilant        restaurant.restaurant_id%TYPE;
-r_Name_dish          restaurant.restaurant_name%TYPE;
-r_Name_complement    restaurant.restaurant_name%TYPE;
-r_Name_seculant      restaurant.restaurant_name%TYPE;
-IS 
+Select Menu_title,
+       Menu_description,
+       Menu_price,
+       Menu_date,
+       Menu_Qty,
+       D.Name_dish,
+       S.Name_Seculant,
+      C.Name_complement
+From Menu M
+   join Dish D
+   on M.id_dish = D.id_dish
+   join Complement C
+   on C.id_complement = M.id_complement
+   join Seculant S
+   on S.id_seculant = M.id_seculant
+   join Restaurant R
+   on R.id_resto = M.id_resto
+WHERE M.Menu_title = '&Menu_title';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+CREATE OR REPLACE PROCEDURE consult_details_menu  IS 
+m_Menu_title         Menu.Menu_title%TYPE;
+m_Descrition_Menu    Menu.Menu_description%TYPE;
+m_menu_price         Menu.Menu_price%TYPE;
+m_Menu_date          Menu.Menu_date%TYPE;
+m_Menu_Qty           Menu.Menu_Qty%TYPE;
+m_name_dish          dish.Name_dish%TYPE;
+m_name_seculant      Seculant.Name_Seculant%TYPE;
+m_palt_complement    complement.Name_complement%TYPE;
+m_name_Resro         restaurant.name_resto%TYPE;
+m_Menu_title2         Menu.Menu_title%TYPE;
+BEGIN 
+SELECT Menu_title
+INTO m_Menu_title2 
+FROM Menu;
+DBMS_OUTPUT.PUT_LINE('Saisir le titre du menu dont vous voulez consulter le detaille :');
+SELECT 
+Menu_title,
+Menu_description,
+Menu_price,
+Menu_date,
+Menu_Qty,
+Name_dish,
+Name_Seculant,
+Name_complement,
+name_resto
+INTO 
+m_Menu_title  ,       
+m_Descrition_Menu ,   
+m_menu_price   ,   
+m_Menu_date ,        
+m_Menu_Qty  ,        
+m_name_dish   ,       
+m_name_seculant ,    
+m_palt_complement ,  
+m_name_Resro 
+FROM Menu M
+JOIN Dish
+ON dish.id_dish = M.id_dish
+JOIN Seculant S
+ON S.id_seculant = M.id_seculant
+JOIN complement C
+ON C.id_complement = M.id_complement
+JOIN restaurant R
+ON R.id_resto = M.id_resto
+WHERE M.Menu_title = '&Menu_title';
+ DBMS_OUTPUT.PUT_LINE('Nom du Menu:'||m_Menu_title||'  '||'Description du Menue:'||m_Descrition_Menu||'  '||'Prix du Menu:'||m_menu_price ||'  '||'date du menu:'||m_Menu_date||' '||'Quantite du Menu:'||m_Menu_Qty ||' '||'Nom du plat :'||m_name_dish||' '||'Nom seculent:'||m_name_seculant||' '||'Nom complement:'||m_palt_complement||' '||'nom du restaurant:'||m_name_Resro);
+END;
+/
 BEGIN
-SELECT
-    id_menu,           
-    Menu_title,         
-    Menu_descrption,   
-    Menu_price,        
-    Menu_date,         
-    Menu_Qty,           
-    id_dish,            
-    id_complement,     
-    id_secilant,       
-    Name_dish,          
-    Name_complement ,  
-    Name_seculant     
-FROM  Menu;
+consult_details_menu;
+END;
+/
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Sweet World/Procedure/Annaelle/consult_restaurant.sql
+++ Sweet World/Procedure/Annaelle/consult_restaurant.sql
@@ -1,12 +1,20 @@
-/*CREATE PROCEDURE consult_restaurant IS 
+SELECT Name_resto,
+       Description,
+       Phone_number,
+       resto_address,
+       Date_res_created
+From Restaurant;
+
+/*CREATE PROCEDURE consult_restaurant 
 r_id_restaurant      restaurant.restaurant_id%TYPE;
 r_Name_resto         restaurant.restaurant_name%TYPE;
 r_Name_employees     restaurant.restaurant_name%TYPE;
 r_Description        restaurant.restaurant_descrption%TYPE;
 r_Phone_number       restaurant.restaurant_number%TYPE;
-r_Resto_adresse      restaurant.restaurant_adresse%TYPE;
+r_Resto_adress      restaurant.restaurant_adresse%TYPE;
 r_Date_res_created   restaurant.restaurant_date%TYPE;
 r_id_user            restaurant.restaurant_id%TYPE;
+IS
 BEGIN 
 SELECT 
     id_restaurant,
@@ -17,65 +25,3 @@ SELECT
     Date_res_created,
     id_user
 FROM  restaurant; */
-
--- CREATE PROCEDURE consult_restaurant 
---     (
---         in_r_id_restaurant int 
-       
---         r_Name_resto         
---         r_Name_employees     
---         r_Description       
---         r_Phone_number       
---         r_Resto_adresse      
---         r_Date_res_created   
---         r_id_user            
---     )
---     IS
---     r_id_restaurant      restaurant.restaurant_id%TYPE;
---     r_Name_resto         restaurant.restaurant_name%TYPE;
---     r_Name_employees     restaurant.restaurant_name%TYPE;
---     r_Description        restaurant.restaurant_descrption%TYPE;
---     r_Phone_number       restaurant.restaurant_number%TYPE;
---     r_Resto_adresse      restaurant.restaurant_adresse%TYPE;
---     r_Date_res_created   restaurant.restaurant_date%TYPE;
---     r_id_user            restaurant.restaurant_id%TYPE;
--- BEGIN 
-
--- SELECT * INTO
---     r_id_restaurant,
---     r_Name_resto,
---     r_Name_employees,
---     r_Description,
---     r_Phone_number,
---     r_Resto_adresse,
---     r_Date_res_created,
---     r_id_user,
-
-    
--- FROM  restaurant
--- WHERE
--- r_id_restaurant    = id-restaurant,
---     r_Name_resto      =  Name_restaurant,
---     r_Name_employees   =  Name_employees,
---     r_Description     = Description,
---     r_Phone_number     = Phone_number,  
---     r_Resto_adresse      = r_Resto_adresse,
---     r_Date_res_created   = Date_res_created,
---     r_id_user            = id_user
-
--- DBMD_OUTPUT.PUT_LINE(r_id_restaurant||''||r_Name_resto||''||r_Name_employees||''||r_Description||''||r_Phone_number||''||r_Resto_adresse||''||r_Date_res_created||''||r_id_user )
--- END;
-
-
-
-
-
---OK
---ou alors, on peut aussi faire ceci
-
-SELECT Name_resto,
-Description,
-Phone_number,
-resto_address,
-Date_res_created
-From Restaurant;

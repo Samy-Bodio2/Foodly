f7ac45cc9a97aa76b49e680807945cc255c55d58
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-01-04 11:13:25 +0100

    ajout de la fonctionnalité de décrémentation

--- Sweet World/Procedure/Awounang/effectuer_commande.sql
+++ Sweet World/Procedure/Awounang/effectuer_commande.sql
@@ -1,5 +1,6 @@
 set serveroutput on;
 
+-- foction pour récupérer l'id du restaurant
 create or replace function id_resto(nom varchar)
 return int IS
 cursor idResto is 
@@ -12,6 +13,7 @@ return id;
 end;
 /
 
+ -- fonction pour récupérer l'id du user du client conserné
 create or replace function id_user(nom1 varchar, nom2 varchar)
 return int is 
 cursor idUser is 
@@ -25,6 +27,7 @@ return id;
 end;
 /
 
+-- function pour récupérer l'id du client
 create or replace function id_cust(nom1 varchar, nom2 varchar)
 return int is 
 cursor idCust is 
@@ -37,6 +40,7 @@ return id;
 end;
 /
 
+-- fonction pour récupérer l'id du panier du client
 create or replace function id_panier(nom1 varchar, nom2 varchar)
 return int is 
 cursor idPanier is 
@@ -50,15 +54,39 @@ end;
 /
 
 declare 
-v_id_resto int :=id_resto('&restaurant');
-v_id_panier int :=id_panier('&username','&password');
-begin 
 
-insert into orders (id_order,order_date,id_resto,id_panier)
-values (id_order_seq.nextval, to_date(sysdate,'dd-mm-yyyy'), v_id_resto, v_id_panier);
-DBMS_OUTPUT.PUT_LINE ('commande effectuée!');
+        v_id_resto int :=id_resto('&restaurant');
+        v_id_panier int :=id_panier('&username','&password');
 
-delete from choix where id_panier = v_id_panier;
+        cursor c_choix is 
+        select id_menu, quantity from choix where id_panier = v_id_panier;
+
+        idM int;
+        qte int ;
+        menuQte int;
+        final_qte int;
+
+
+begin
+
+        --creation de la commande
+        insert into orders (id_order,order_date,id_resto,id_panier)
+        values (id_order_seq.nextval, to_date(sysdate,'dd-mm-yyyy'), v_id_resto, v_id_panier);
+        DBMS_OUTPUT.PUT_LINE ('commande effectuée!');
+
+        --vidange du panier du client
+        delete from choix where id_panier = v_id_panier;
+
+        --décrementation de la qte du menu chez le restaurateur
+        open c_choix;
+        loop
+        fetch c_choix into idM, qte;
+        exit when c_choix%notfound;
+        select menu_qty into menuQte from menu where id_menu = idM;
+        final_qte := menuQte - qte;
+        update menu 
+        set menu_qty = final_qte;
+        end loop;
 
 end;
 /

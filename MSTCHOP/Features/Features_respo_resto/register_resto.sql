3a78665a406b9051045cfee94937ec1ff855513f
JasonKsj <jasonkamsu@gmail.com>
2023-01-05 10:22:35 +0100

    Case

--- MSTCHOP/Features/Features_respo_resto/register_resto.sql
+++ MSTCHOP/Features/Features_respo_resto/register_resto.sql
@@ -1,3 +1,12 @@
+SET LINESIZE 6000
+BEGIN
+sp_register_admin (p_id_user => id_user_seq.NEXTVAL,
+             p_username => '&username',   
+             p_password => format1('&password')                      
+             );
+END;
+/
+
 INSERT INTO Restaurant(id_resto,name_resto,Description,email,phone_number,resto_address,id_user)
 VALUES
 (id_resto_seq.nextval,
@@ -6,6 +15,8 @@ VALUES
 '&email',
 '&Numero',
 '&Adresse_du_restaurant'
-,id_user('&Enter_your_Username'));
+,idutil('&Enter_your_Username'));
+
+SELECT * FROM Restaurant c JOIN Users u ON (c.id_user = u.id_user);
 
 @Features/Features_respo_resto/Menu_respo_resto_con
--- MSTCHOP/Procedure/Inscription.sql
+++ MSTCHOP/Procedure/Inscription.sql
@@ -18,7 +18,7 @@ column script new_value v_script
 select  case '&selection'
         when '1' then '@Features/Features_admin/register_admin'
         when '2' then '@Features/Features_respo_resto/register_resto'
-        when '3' then '@Features/Features_customer/register_customer'
+        when '3' then '@Features/Features_customer/register_cust'
         else 'Procedure/Inscription'
         end as script
 from dual;

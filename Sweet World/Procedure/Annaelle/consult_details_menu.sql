c3c78658493e4d93a3c228454dbdf30c314f2e3f
annaellejosee13 <annaellejoseemegane@gmail.com>
2023-01-05 17:22:05 +0100

    ajout

--- Sweet World/Procedure/Annaelle/consult_details_menu.sql
+++ Sweet World/Procedure/Annaelle/consult_details_menu.sql
@@ -1,21 +1,21 @@
-Select Menu_title,
+SELECT Menu_title,
        Menu_description,
        Menu_price,
        Menu_date,
        Menu_Qty,
-       D.Name_dish,
-       S.Name_Seculant,
-      C.Name_complement
-From Menu M
-   join Dish D
-   on M.id_dish = D.id_dish
-   join Complement C
-   on C.id_complement = M.id_complement
-   join Seculant S
-   on S.id_seculant = M.id_seculant
-   join Restaurant R
-   on R.id_resto = M.id_resto
-WHERE M.Menu_title = '&Menu_title';
+       d.Name_dish,
+       s.Name_Seculant,
+       g.Name_garnish
+FROM Menu m
+   JOIN Dish d
+   ON m.id_dish = d.id_dish
+   JOIN Garnish g
+   ON g.id_garnish= m.id_garnish
+   JOIN Seculant s
+   ON s.id_seculant = m.id_seculant
+   JOIN Restaurant r
+   ON r.id_resto = m.id_resto
+WHERE m.Menu_title = '&Menu_title';
 
 
 
@@ -35,9 +35,9 @@ WHERE M.Menu_title = '&Menu_title';
 
 
 /*
-CREATE OR REPLACE PROCEDURE consult_details_menu  IS 
+CREATE OR REPLACE PROCEDURE cONsult_details_menu  IS 
 m_Menu_title         Menu.Menu_title%TYPE;
-m_Descrition_Menu    Menu.Menu_description%TYPE;
+m_DescritiON_Menu    Menu.Menu_descriptiON%TYPE;
 m_menu_price         Menu.Menu_price%TYPE;
 m_Menu_date          Menu.Menu_date%TYPE;
 m_Menu_Qty           Menu.Menu_Qty%TYPE;
@@ -50,10 +50,10 @@ BEGIN
 SELECT Menu_title
 INTO m_Menu_title2 
 FROM Menu;
-DBMS_OUTPUT.PUT_LINE('Saisir le titre du menu dont vous voulez consulter le detaille :');
+DBMS_OUTPUT.PUT_LINE('Saisir le titre du menu dONt vous voulez cONsulter le detaille :');
 SELECT 
 Menu_title,
-Menu_description,
+Menu_descriptiON,
 Menu_price,
 Menu_date,
 Menu_Qty,
@@ -63,7 +63,7 @@ Name_complement,
 name_resto
 INTO 
 m_Menu_title  ,       
-m_Descrition_Menu ,   
+m_DescritiON_Menu ,   
 m_menu_price   ,   
 m_Menu_date ,        
 m_Menu_Qty  ,        
@@ -81,11 +81,11 @@ ON C.id_complement = M.id_complement
 JOIN restaurant R
 ON R.id_resto = M.id_resto
 WHERE M.Menu_title = '&Menu_title';
- DBMS_OUTPUT.PUT_LINE('Nom du Menu:'||m_Menu_title||'  '||'Description du Menue:'||m_Descrition_Menu||'  '||'Prix du Menu:'||m_menu_price ||'  '||'date du menu:'||m_Menu_date||' '||'Quantite du Menu:'||m_Menu_Qty ||' '||'Nom du plat :'||m_name_dish||' '||'Nom seculent:'||m_name_seculant||' '||'Nom complement:'||m_palt_complement||' '||'nom du restaurant:'||m_name_Resro);
+ DBMS_OUTPUT.PUT_LINE('Nom du Menu:'||m_Menu_title||'  '||'DescriptiON du Menue:'||m_DescritiON_Menu||'  '||'Prix du Menu:'||m_menu_price ||'  '||'date du menu:'||m_Menu_date||' '||'Quantite du Menu:'||m_Menu_Qty ||' '||'Nom du plat :'||m_name_dish||' '||'Nom seculent:'||m_name_seculant||' '||'Nom complement:'||m_palt_complement||' '||'nom du restaurant:'||m_name_Resro);
 END;
 /
 BEGIN
-consult_details_menu;
+cONsult_details_menu;
 END;
 /
 

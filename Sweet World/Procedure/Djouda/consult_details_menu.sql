307cc6fed9194ab27a98aff92e64da45139ec0bc
patric237 <djoudapatric61@gmail.com>
2023-01-07 12:55:15 +0100

    Update consult_details_menu.sql

--- Sweet World/Procedure/Djouda/consult_details_menu.sql
+++ Sweet World/Procedure/Djouda/consult_details_menu.sql
@@ -7,7 +7,7 @@ v_Menu_date          Menu.Menu_date%TYPE;
 v_Menu_Qty           Menu.Menu_Qty%TYPE;
 V_name_dish          dish.Name_dish%TYPE;
 V_name_seculant      Seculant.Name_Seculant%TYPE;
-V_palt_complement    complement.Name_complement%TYPE;
+V_palt_Name_garnish    Name_garnish.Name_garnish%TYPE;
 V_name_Resro         restaurant.name_resto%TYPE;
 BEGIN 
 SELECT 
@@ -18,7 +18,7 @@ Menu_date,
 Menu_Qty,
 Name_dish,
 Name_Seculant
-Name_complement,
+Name_garnish,
 name_resto
 INTO 
 V_Menu_title  ,       
@@ -28,15 +28,15 @@ v_Menu_date ,
 v_Menu_Qty  ,        
 V_name_dish   ,       
 V_name_seculant ,    
-V_palt_complement ,  
+V_palt_Name_garnish ,  
 V_name_Resro 
 FROM Menu m
 JOIN Dish d
 ON d.id_dish = m.id_dish
 JOIN Seculant s 
 ON s.id_seculant = m.id_seculant
-JOIN complement c
-ON c.id_complement = m.id_complement
+JOIN Name_garnish c
+ON c.id_Name_garnish = m.id_Name_garnish
 JOIN restaurant r
 ON r.id_resto = m.id_resto;
 END;

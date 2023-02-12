23656ada37748d7a38d426d0a95ec2743e01c319
annaellejosee13 <annaellejoseemegane@gmail.com>
2023-02-13 00:25:43 +0100

    Comments Customer First Name & Last name

--- MSTCHOP/Object/Comments/Comment_customers.sql
+++ MSTCHOP/Object/Comments/Comment_customers.sql
@@ -5,10 +5,10 @@ COMMENT ON COLUMN Customers.id_cust
 IS 'Primary key of Customers table.'
 
 COMMENT ON COLUMN Customers.first_name  
-IS 'Customer Name.'
+IS 'Customer Fisrt Name.'
 
 COMMENT ON COLUMN Customers.last_name 
-IS 'Prenom du client.'
+IS 'Customer Last Name.'
 
 COMMENT ON COLUMN Customers.phone_number 
 IS 'Numero du client.'

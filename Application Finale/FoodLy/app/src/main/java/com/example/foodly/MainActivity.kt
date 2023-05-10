3ee17c1dd6aa9edba6dde1e0716ba1fbbd71cd5f
Samy-Bodio2 <samybodio8@gmail.com>
2023-05-10 19:10:09 +0200

    map

--- Application Finale/FoodLy/app/src/main/java/com/example/foodly/MainActivity.kt
+++ Application Finale/FoodLy/app/src/main/java/com/example/foodly/MainActivity.kt
@@ -5,18 +5,20 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.foodly.Customers.Splash.navigation.Screen
-import com.example.foodly.Inscription.Email
+/*import com.example.foodly.Inscription.Email
 import com.example.foodly.Inscription.Name
 import com.example.foodly.Inscription.Password
-import com.example.foodly.Inscription.Phone_number
+import com.example.foodly.Inscription.Phone_number*/
 
 
 import com.example.foodly.Splash.AnimatedSplashScreen
 import com.example.foodly.Splash.PageViewScreen
+import com.example.foodly.commander.DesignMap
 import com.example.foodly.commander.FoodOrderScreen
 import com.example.foodly.commander.facturation
 import com.example.foodly.commander.momoOM
@@ -45,14 +47,14 @@ class MainActivity : ComponentActivity() {
     fun LoginApplication(){
         val navController = rememberNavController()
 
-        NavHost(navController = navController, startDestination = Screen.Splash.route, builder = {
+        NavHost(navController = navController, startDestination = "mapF", builder = {
             composable(route = Screen.Splash.route){ AnimatedSplashScreen(navController) }
             composable(route = Screen.PageScreen.route){PageViewScreen(navController) }
-            composable("name", content = { Name(navController = navController) })
+            /*composable("name", content = { Name(navController = navController) })
             composable("email", content = { Email(navController = navController)} )
             composable("phone_number", content = { Phone_number(navController = navController) })
-            composable("password", content = { Password(navController = navController) })
-            composable("login_page", content = { LoginPage(null, navController = navController) })
+            composable("password", content = { Password(navController = navController) })*/
+            //composable("login_page", content = { LoginPage(null, navController = navController) })
             composable("homescreen", content = { HomeScreen(null, navController = navController)} )
             composable("register_page", content = { RegisterPage(navController = navController) })
             composable("reset_page", content = { ResetPage(navController = navController) })
@@ -61,6 +63,7 @@ class MainActivity : ComponentActivity() {
             composable("passComm",content={FoodOrderScreen(navController = navController) })
             composable("factu", content = { facturation(navController = navController) })
             composable("momoOM", content = { momoOM(navController = navController) })
+            composable("mapF", content = { DesignMap(context = this@MainActivity, navController = navController) })
         })
     }
 }

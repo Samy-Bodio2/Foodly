6058c728c7451ed11ad005ef749f9e42d08feb66
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-05-15 17:03:12 +0200

    interface de categorieItem

--- Application Finale/FoodLy/app/src/main/java/com/example/foodly/model/Meal.kt
+++ Application Finale/FoodLy/app/src/main/java/com/example/foodly/model/Meal.kt
@@ -1,2 +1,64 @@
 package com.example.foodly.model
 
+import androidx.annotation.DrawableRes
+import com.example.foodly.R
+
+data class Meal (
+    val nom :String,
+    val description: String,
+    val prix: Int,
+    val note : Float,
+    @DrawableRes val image: Int
+)
+
+
+var mealList = listOf(
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    ),
+    Meal("Cheese Burger",
+        "",
+        3500,
+        4.6f,
+        R.drawable.burger2
+    )
+)

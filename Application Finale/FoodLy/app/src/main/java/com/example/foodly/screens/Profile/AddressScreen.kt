e7226a2673285eda15b8ea87a015eeabe19b738c
Djouda patric <98420343+djoudapatric@users.noreply.github.com>
2023-05-16 08:12:17 +0100

    ecriture dans adresse screen

--- Application Finale/FoodLy/app/src/main/java/com/example/foodly/screens/Profile/AddressScreen.kt
+++ Application Finale/FoodLy/app/src/main/java/com/example/foodly/screens/Profile/AddressScreen.kt
@@ -1,2 +1,41 @@
 package com.example.foodly.screens.Profile
 
+import android.icu.text.ListFormatter.Width
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.RemoveRedEye
+import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.size.Size
+import com.example.foodly.R
+import com.example.foodly.ui.theme.LightGreen
+import com.example.foodly.ui.theme.LightGreen2
+import com.example.foodly.ui.theme.colorWhite
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+
+@Preview
+@Composable
+fun PreviewAdresse(){
+    AdresseScreen()
+}

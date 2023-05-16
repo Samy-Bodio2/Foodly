05c2f0a431b6f5ffb592fe0c7aac714ace039662
Djouda patric <98420343+djoudapatric@users.noreply.github.com>
2023-05-16 08:21:13 +0100

    ecriture dans Notification screen

--- Application Finale/FoodLy/app/src/main/java/com/example/foodly/screens/Profile/NotificationsScreen.kt
+++ Application Finale/FoodLy/app/src/main/java/com/example/foodly/screens/Profile/NotificationsScreen.kt
@@ -1,2 +1,25 @@
 package com.example.foodly.screens.Profile
 
+
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.RemoveRedEye
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+
+
+@Preview
+@Composable
+fun PreviewNotification(){
+    NotificationScreen()
+}

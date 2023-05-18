8be749a931a23e75bbbafd8cb3934f407829249c
Jordan-Tchantchou <angelenguiakam@gmail.com>
2023-05-18 17:14:12 +0100

    importation

--- Application Finale/FoodLy/app/src/main/java/com/example/foodly/screens/Profile/PayementMethodsProfile.kt
+++ Application Finale/FoodLy/app/src/main/java/com/example/foodly/screens/Profile/PayementMethodsProfile.kt
@@ -3,7 +3,6 @@ package com.example.foodly.screens.Profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,6 +19,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.foodly.R
 
 
 @Composable
